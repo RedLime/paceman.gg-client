f8ce935e726e5b557e3683695e59c5431b841737
RedLime <redlimerl@gmail.com>
2023-07-27 00:59:35 +0900

    cleanup

--- src/main/java/gg/paceman/client/PacemanGG.java
+++ src/main/java/gg/paceman/client/PacemanGG.java
@@ -1,14 +1,13 @@
 package gg.paceman.client;
 
 import com.redlimerl.speedrunigt.option.OptionArgument;
-import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.util.Identifier;
 
 import java.util.Objects;
 
-public class PacemanGG implements ClientModInitializer {
+public class PacemanGG {
 
     public static final ModContainer MOD_CONTAINER = FabricLoader.getInstance().getModContainer("paceman_gg").orElseThrow(RuntimeException::new);
     public static final OptionArgument<Boolean> ENABLE_PACEMAN_GG_LIVE = new OptionArgument<Boolean>(new Identifier(MOD_CONTAINER.getMetadata().getId(), "paceman_gg_live"), false) {
@@ -22,9 +21,4 @@ public class PacemanGG implements ClientModInitializer {
             return String.valueOf(value);
         }
     };
-
-    @Override
-    public void onInitializeClient() {
-
-    }
 }
