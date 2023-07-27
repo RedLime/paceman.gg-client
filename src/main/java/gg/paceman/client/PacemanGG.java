db8b47b3ba236b7ac302dce00ae9cf6548184dbb
RedLime <redlimerl@gmail.com>
2023-07-27 11:25:00 +0900

    migrate https / cleanups

--- src/main/java/gg/paceman/client/PacemanGG.java
+++ src/main/java/gg/paceman/client/PacemanGG.java
@@ -1,24 +1,121 @@
 package gg.paceman.client;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.redlimerl.speedrunigt.SpeedRunIGT;
 import com.redlimerl.speedrunigt.option.OptionArgument;
+import com.redlimerl.speedrunigt.timer.InGameTimer;
+import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
+import com.redlimerl.speedrunigt.timer.running.RunType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.metadata.ModOrigin;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.util.Identifier;
+import org.apache.commons.io.IOUtils;
 
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class PacemanGG {
 
-    public static final ModContainer MOD_CONTAINER = FabricLoader.getInstance().getModContainer("paceman_gg").orElseThrow(RuntimeException::new);
-    public static final OptionArgument<Boolean> ENABLE_PACEMAN_GG_LIVE = new OptionArgument<Boolean>(new Identifier(MOD_CONTAINER.getMetadata().getId(), "paceman_gg_live"), false) {
-        @Override
-        public Boolean valueFromString(String string) {
-            return Objects.equals(string, "true");
+    private static final String KEY_BASE_URL = "https://paceman.gg/api/submitrun";
+    private static final ExecutorService threadExecutor = Executors.newSingleThreadExecutor();
+
+    private static void setupConnection(HttpURLConnection connection) {
+        connection.setConnectTimeout(10000);
+        connection.setReadTimeout(10000);
+        connection.setRequestProperty("User-Agent", "paceman.gg-client/"+ SpeedRunIGT.MOD_VERSION);
+        connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+    }
+
+    public static void updateTimerData(InGameTimer timer, boolean reset) {
+
+        // Skip these all things lol
+        if (!timer.isStarted() || timer.isOpenedIntegratedServer() || !SpeedRunIGT.IS_CLIENT_SIDE
+                || timer.isRTAMode() || timer.getRunType() == RunType.OLD_WORLD
+                || timer.isCheatAvailable() || timer.getDefaultGameMode() != 0) {
+            return;
         }
 
-        @Override
-        public String valueToString(Boolean value) {
-            return String.valueOf(value);
+        JsonObject payloadData = convertJson(timer, reset);
+
+        threadExecutor.submit(() -> {
+            try {
+                URL url = new URL(KEY_BASE_URL);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                setupConnection(connection);
+                connection.setRequestMethod("POST");
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+                bw.write(SpeedRunIGT.GSON.toJson(payloadData));
+                bw.flush();
+                bw.close();
+
+                int result = connection.getResponseCode();
+                SpeedRunIGT.debug("paceman.gg status > " + result);
+                InputStream in;
+                if (result >= 200 && result < 300) {
+                    in = connection.getInputStream();
+                    SpeedRunIGT.debug(IOUtils.toString(in, StandardCharsets.UTF_8));
+                } else {
+                    in = connection.getErrorStream();
+                    SpeedRunIGT.error(IOUtils.toString(in, StandardCharsets.UTF_8));
+                }
+                in.close();
+            } catch (SocketTimeoutException e) {
+                SpeedRunIGT.error("Failed to upload timer data on therun.gg cause by timeout");
+            } catch (Exception e) {
+                e.printStackTrace();
+                SpeedRunIGT.error("Failed to upload timer data on therun.gg");
+            }
+        });
+
+    }
+
+    private static JsonObject convertJson(InGameTimer timer, boolean reset) {
+        JsonObject result = new JsonObject();
+
+        if (!reset) {
+            result.add("record", InGameTimerUtils.convertTimelineJson(timer));
         }
-    };
+
+        JsonArray nicknames = new JsonArray();
+        JsonArray uuids = new JsonArray();
+        if (MinecraftClient.getInstance().getNetworkHandler() != null) {
+            for (PlayerListEntry playerListEntry : MinecraftClient.getInstance().getNetworkHandler().getPlayerList()) {
+                nicknames.add(playerListEntry.getProfile().getName());
+                uuids.add(playerListEntry.getProfile().getId().toString());
+            }
+        }
+        result.add("nicknames", nicknames);
+        result.add("uuids", uuids);
+
+        JsonArray mods = new JsonArray();
+        for (ModContainer mod : FabricLoader.getInstance().getAllMods()) {
+            if (mod.getOrigin().getKind() == ModOrigin.Kind.PATH) {
+                mods.add(mod.getMetadata().getId());
+            }
+        }
+        result.add("modlist", mods);
+
+        JsonArray twitch = new JsonArray();
+        JsonArray alt = new JsonArray();
+        result.add("twitch", twitch);
+        result.add("alt", alt);
+
+        return result;
+    }
+
 }
--- src/main/java/gg/paceman/client/mixin/MixinInGameTimer.java
+++ src/main/java/gg/paceman/client/mixin/MixinInGameTimer.java
@@ -1,7 +1,7 @@
 package gg.paceman.client.mixin;
 
 import com.redlimerl.speedrunigt.timer.InGameTimer;
-import gg.paceman.client.PacemanRequestHelper;
+import gg.paceman.client.PacemanGG;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,21 +16,21 @@ public class MixinInGameTimer {
 
     @Inject(method = "complete(JZ)V", at = @At(value = "INVOKE", target = "Lcom/redlimerl/speedrunigt/therun/TheRunRequestHelper;submitTimerData(Lcom/redlimerl/speedrunigt/timer/InGameTimer;)V", shift = At.Shift.AFTER, remap = false), remap = false)
     private static void onComplete(CallbackInfo ci) {
-        PacemanRequestHelper.updateTimerData(INSTANCE, false);
+        PacemanGG.updateTimerData(INSTANCE, false);
     }
 
     @Inject(method = "leave", at = @At(value = "INVOKE", target = "Lcom/redlimerl/speedrunigt/therun/TheRunRequestHelper;updateTimerData(Lcom/redlimerl/speedrunigt/timer/InGameTimer;Lcom/redlimerl/speedrunigt/therun/TheRunTimer$PacketType;)V", shift = At.Shift.AFTER, remap = false), remap = false)
     private static void onLeave(CallbackInfo ci) {
-        PacemanRequestHelper.updateTimerData(INSTANCE, true);
+        PacemanGG.updateTimerData(INSTANCE, true);
     }
 
     @Inject(method = "setPause(ZLcom/redlimerl/speedrunigt/timer/TimerStatus;Ljava/lang/String;)V", at = @At(value = "INVOKE", target = "Lcom/redlimerl/speedrunigt/therun/TheRunRequestHelper;updateTimerData(Lcom/redlimerl/speedrunigt/timer/InGameTimer;Lcom/redlimerl/speedrunigt/therun/TheRunTimer$PacketType;)V", shift = At.Shift.AFTER, remap = false), remap = false)
     private void onPause(CallbackInfo ci) {
-        PacemanRequestHelper.updateTimerData(INSTANCE, false);
+        PacemanGG.updateTimerData(INSTANCE, false);
     }
 
     @Inject(method = "tryInsertNewAdvancement", at = @At(value = "INVOKE", target = "Lcom/redlimerl/speedrunigt/timer/TimerAdvancementTracker$AdvancementTrack;setComplete(Z)V", shift = At.Shift.AFTER, remap = false), remap = false)
     private void onCompleteAdvancement(String advancementID, String criteriaKey, boolean isAdvancement, CallbackInfo ci) {
-        if (isAdvancement) PacemanRequestHelper.updateTimerData(INSTANCE, false);
+        if (isAdvancement) PacemanGG.updateTimerData(INSTANCE, false);
     }
 }
